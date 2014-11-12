0b8ef44fe5dc8c5713f4dad0925d7b9c1f57eab8
Rickard Jonsson <olji@hotmail.se>
2014-11-12 15:47:16 +0100

    Changed some things in handler

--- src/utilities/InputHandler.cpp
+++ src/utilities/InputHandler.cpp
@@ -5,7 +5,7 @@
 
 void InputHandler::keyboardUpdate(){
     releasedKeys_.clear();
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){ //500 To be changed to variable y coords
+    if (pressedKeys_.find(sf::Keyboard::Key::Up) != pressedKeys_.end()){
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){ //500 To be changed to variable y coords
     }
@@ -17,5 +17,8 @@ void InputHandler::keyboardUpdate(){
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
     }
+}
+bool isKeyPressed(sf::Keyboard::Key key){
+    return true;
 }
 #endif
