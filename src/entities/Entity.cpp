d3585ed5e57023c33ef5b18a32ede9f0a0169a7c
RiverMountain <rickard@r-aberg.se>
2014-11-13 13:05:52 +0100

    no differensce interuppted by som dudes....

--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -27,11 +27,12 @@ void Entity::draw(sf::RenderWindow& window){
 }
 void Unit::getMovement(){
     if (kb.isKeyPressed(sf::Keyboard::Up) && position_.y >= 500){ //500 To be changed to variable y coords
+        state_ = unitState::falling;
         momentum_.y = -20.0f;
         if(doUnitLookLeft())
-            momentum_.x = 20.0f;
+            momentum_.x = -10.0f;
         else
-            momentum_.x = 20.0f;
+            momentum_.x = 10.0f;
         std::cout << "Up" << std::endl;
     } else { 
         if(momentum_.y < 0) 
@@ -41,13 +42,15 @@ void Unit::getMovement(){
     }
     if (kb.isKeyPressed(sf::Keyboard::Left) && -momentum_.x <= maxMomentum_.x){
         momentum_.x += -speed_;
-        std::cout << "Left" << std::endl;
+        unitLookLeftNow(true); 
+        std::cout << "Left, look at left?: " << doUnitLookLeft() << std::endl;
     }
     else if (kb.isKeyPressed(sf::Keyboard::Left))
 	    momentum_.x = -maxMomentum_.x;
     if (kb.isKeyPressed(sf::Keyboard::Right) && momentum_.x <= maxMomentum_.x){
-        std::cout << "Right" << std::endl;
         momentum_.x += speed_;
+        unitLookLeftNow(false);
+        std::cout << "Right, look at left?: " << doUnitLookLeft() << std::endl;
     }
     else if (kb.isKeyPressed(sf::Keyboard::Right) && momentum_.x <= maxMomentum_.x)
 	    momentum_.x = maxMomentum_.x;
@@ -81,7 +84,10 @@ void Unit::getMovement(){
 }
 
 void Unit::applyPhysics(){
-    Entity::applyPhysics();
+    if(position_.y <= 500)
+        Entity::applyPhysics();
+    else
+        Entity::applyPhysics();
 }
 void Unit::move(){
     Entity::move();
