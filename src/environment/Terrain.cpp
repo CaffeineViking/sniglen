430b74f17c653b82978548f4efa882368d39a8e2
Erik Jansson <caffeineviking@gmail.com>
2014-11-17 16:30:08 +0100

    Added Terrain documentation.

--- src/environment/Terrain.cpp
+++ src/environment/Terrain.cpp
@@ -5,19 +5,24 @@
 #include <iostream>
 
 Terrain::Terrain(unsigned size) {
+    // Allocate a image with the desired size of the terrain.
     image_.create(size, 540, {0, 0, 0, 0});
     const PerlinNoise perlin{Random::GENERATE()};
 
+    // These change the frequency of the "bumps" on the terrrain.
     const double frequency{(size / 1024) * 2.5f};
     const double frequencyX{image_.getSize().x / frequency};
 
+    // A texture will overlay the generated terrain, load this.
     sf::Image terrainTexture;
     terrainTexture.loadFromFile("share/terrain.png");
 
+    // For every pixel in width, generate a noise "height".
     for (size_t x{0}; x < image_.getSize().x; ++x) {
         const double noise{(perlin.octaveNoise(x / frequencyX, 3) + 1.0) / 2.0};
         data_.push_back(noise * image_.getSize().y);
 
+        // Also, copy pixels from the terrain overlay to every area below the generated height.
         for (size_t y{noise * image_.getSize().y}; y < image_.getSize().y; ++y) {
             size_t xMod{x % terrainTexture.getSize().x};
             size_t yMod{y % terrainTexture.getSize().y};
@@ -25,6 +30,7 @@ Terrain::Terrain(unsigned size) {
                 terrainTexture.getPixel(xMod, yMod).g,
                 terrainTexture.getPixel(xMod, yMod).b};
 
+            // Check if we need to generate the beautiful terrain outline.
             if (y < noise * image_.getSize().y + 10) {
                 image_.setPixel(x, y, {96, 83, 66});
             } else {
@@ -43,9 +49,11 @@ void Terrain::draw(sf::RenderWindow& window) const {
 }
 
 bool Terrain::isColliding(const Entity& entity) const {
+    // This initial check here saves us the trouble from out of bounds errors.
     if (entity.getPos().x < 0 || entity.getPos().x > image_.getSize().x) {
         return false;
     } else {
+        // See if part of the entity is below the generated terrain height.
         return entity.getPos().y > (sprite_.getPosition().y + data_[entity.getPos().x]);
     }
 }
--- src/environment/Terrain.hpp
+++ src/environment/Terrain.hpp
@@ -5,11 +5,16 @@
 #include <SFML/Graphics.hpp>
 #include "../entities/Entity.hpp"
 
+// Provides a two dimensional, randomly generated terrain
+// using Perlin Noise. Additionaly, collision between
+// Terrain and Entity can be checked (via pixel collision).
+
 class Terrain {
 public:
     Terrain(unsigned);
-    void draw(sf::RenderWindow&) const;
-    bool isColliding(const Entity&) const;
+    void draw(sf::RenderWindow&) const; // Draws the generated terrain sprite to the main window.
+    bool isColliding(const Entity&) const; // Checks the generated Perlin Noise function if a
+    // intersection between the curve function and the entity is happening.
 
 private:
     sf::Sprite sprite_;
