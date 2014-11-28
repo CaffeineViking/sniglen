b4d477fe443c238d4e687546d07bb06371c64955
Rickard Jonsson <olji@hotmail.se>
2014-11-28 16:19:18 +0100

    Removed compiler warnings

--- src/MenuScreen.cpp
+++ src/MenuScreen.cpp
@@ -1,13 +1,21 @@
 #include "MenuScreen.hpp"
 
+MenuScreen::MenuScreen(sf::RenderWindow& screen, InputHandler& handler): window{&screen}, input{&handler} {
+    buttonVector.push_back(std::make_pair("Start", new sf::Sprite(Assets::LOAD_TEXTURE("test2.jpg"))));
+    buttonVector.back().second->setOrigin(buttonVector.back().second->getTexture()->getSize().x/2, buttonVector.back().second->getTexture()->getSize().y/2);
+    buttonVector.back().second->setPosition({(float)window->getSize().x/2, (float)window->getSize().y/2});
+    buttonVector.push_back(std::make_pair("Not Start", new sf::Sprite(Assets::LOAD_TEXTURE("test3.jpg"))));
+    buttonVector.back().second->setOrigin(buttonVector.back().second->getTexture()->getSize().x/2, buttonVector.back().second->getTexture()->getSize().y/2);
+    buttonVector.back().second->setPosition({(float)window->getSize().x/2 + 100, (float)window->getSize().y/2});
+}
 void MenuScreen::update(){
     if(input->mouseReleased()){
-       for(std::pair<std::string, sf::Sprite*> button : buttonVector){
-           if(buttonClicked(*button.second)){
-               if(button.first == "Start")
-                   gameStart = true;
-           }
-       }
+        for(std::pair<std::string, sf::Sprite*> button : buttonVector){
+            if(buttonClicked(*button.second)){
+                if(button.first == "Start")
+                    gameStart = true;
+            }
+        }
     }
 }
 void MenuScreen::draw(){
@@ -17,9 +25,9 @@ void MenuScreen::draw(){
 }
 bool MenuScreen::buttonClicked(const sf::Sprite& sprite){
     if(input->mousePosition().x > sprite.getPosition().x - sprite.getTexture()->getSize().x/2 &&
-       input->mousePosition().x < sprite.getPosition().x + sprite.getTexture()->getSize().x/2 &&
-       input->mousePosition().y > sprite.getPosition().y - sprite.getTexture()->getSize().y/2 &&
-       input->mousePosition().y < sprite.getPosition().y + sprite.getTexture()->getSize().y/2)
+            input->mousePosition().x < sprite.getPosition().x + sprite.getTexture()->getSize().x/2 &&
+            input->mousePosition().y > sprite.getPosition().y - sprite.getTexture()->getSize().y/2 &&
+            input->mousePosition().y < sprite.getPosition().y + sprite.getTexture()->getSize().y/2)
         return true;
     return false;
 }
--- src/MenuScreen.hpp
+++ src/MenuScreen.hpp
@@ -10,20 +10,13 @@
 
 class MenuScreen{
     private:
-        InputHandler* input;
         sf::RenderWindow* window;
+        InputHandler* input;
         std::vector<std::pair<std::string, sf::Sprite*>> buttonVector;
         bool gameStart = false;
         bool buttonClicked(const sf::Sprite&);
     public:
-        MenuScreen(sf::RenderWindow& screen, InputHandler& handler): window{&screen}, input{&handler} {
-            buttonVector.push_back(std::make_pair("Start", new sf::Sprite(Assets::LOAD_TEXTURE("test2.jpg"))));
-            buttonVector.back().second->setOrigin(buttonVector.back().second->getTexture()->getSize().x/2, buttonVector.back().second->getTexture()->getSize().y/2);
-            buttonVector.back().second->setPosition({window->getSize().x/2, window->getSize().y/2});
-            buttonVector.push_back(std::make_pair("Not Start", new sf::Sprite(Assets::LOAD_TEXTURE("test3.jpg"))));
-            buttonVector.back().second->setOrigin(buttonVector.back().second->getTexture()->getSize().x/2, buttonVector.back().second->getTexture()->getSize().y/2);
-            buttonVector.back().second->setPosition({window->getSize().x/2 + 100, window->getSize().y/2});
-        };
+        MenuScreen(sf::RenderWindow& screen, InputHandler& handler);
         void update();
         void draw();
         bool getGameStart();
