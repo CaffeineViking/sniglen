7acdb5c363440f48ada566d0508687d4ee233641
Rickard Jonsson <olji@hotmail.se>
2014-12-05 16:02:15 +0100

    Added support to load fonts

--- src/utilities/Assets.hpp
+++ src/utilities/Assets.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Font.hpp>
 #include <SFML/Audio/SoundBuffer.hpp>
 #include <SFML/Audio/Music.hpp>
 
@@ -12,6 +13,7 @@ public:
     static const sf::Texture& LOAD_TEXTURE(const std::string&);
     static const sf::SoundBuffer& LOAD_SOUND(const std::string&);
     static sf::Music& LOAD_MUSIC(const std::string&);
+    static const sf::Font& LOAD_FONT(const std::string&);
 
 private:
     static const std::string PATH;
@@ -22,6 +24,7 @@ private:
     static std::unordered_map<std::string, sf::Texture> TEXTURES_;
     static std::unordered_map<std::string, sf::SoundBuffer> SOUNDS_;
     static std::unordered_map<std::string, sf::Music> MUSIC_;
+    static std::unordered_map<std::string, sf::Font> FONTS_;
 };
 
 #endif
